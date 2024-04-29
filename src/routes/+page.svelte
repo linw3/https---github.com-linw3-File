dbfd7fb6fd1d45872b5c6a38119255954289fd71
unknown <3134119316@qq.com>
2024-04-29 18:03:29 +0800

    uploadFile'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // @ts-nocheck
+
+  import { onMount } from "svelte";
+
+  /**
+   * @type {string | Blob | null}
+   */
+  let selectedFile = null;
+
+  /**
+   * @param {{ target: { files: (string | Blob | null)[]; }; }} event
+   */
+  function handleFileSelect(event) {
+    selectedFile = event.target.files[0];
+    console.log(selectedFile);
+  }
+
+  function uploadFile() {
+    if (selectedFile) {
+      console.log(selectedFile);
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+      fetch("/upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("File uploaded successfully:", data);
+        })
+        .catch((error) => {
+          console.error("File upload failed:", error);
+        });
+    }
+  }
+
+  onMount(() => {
+    console.log("FileUploader component mounted");
+  });
+</script>
+
+<h1>File Uploader</h1>
+
+<input type="file" on:change={handleFileSelect} />
+
+<button on:click={uploadFile}>Upload</button>
